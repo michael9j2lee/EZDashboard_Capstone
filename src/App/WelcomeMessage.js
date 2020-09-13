273bc04301215821b986ae775cdc625fb70c2042
michael lee <michaelee@csumb.edu>
2020-09-13 11:52:23 -0700

    Now using yarn, added styled components, with practicing.

---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Welcome(props) {
+    return <h1>Hello, Welcome to EZDashboard!</h1>
+  }
--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import './App.css';
+import Welcome from './WelcomeMessage'
+import styled, {css} from 'styled-components'
+
+const MyButton= styled.button`
+  color:green;
+  ${props => props.primary && css`
+    background: black;
+    color: palevioletred;
+  `}
+`
+
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
 
 function App() {
   return (
-    <div> Hello World</div>
+    <div>
+      <Welcome/>
+      <MyButton> Hello </MyButton><br></br>
+      <MyButton primary> Hello </MyButton><br></br>
+      <TomatoButton primary> Hello </TomatoButton><br></br>
+    </div>
   );
 }
 
