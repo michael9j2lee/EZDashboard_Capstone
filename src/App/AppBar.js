335cc2ad3c33d1a8142ae50dd9eaaf1d31c20406
michael lee <michaelee@csumb.edu>
2020-09-13 12:16:46 -0700

    Styled App Bar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,16 +1,45 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+
+const Logo = styled.div`
+    font-size: 1.5em;
+`
 
 const Bar = styled.div`
     display: grid;
+    margin-bottom: 40px;
     grid-template-columns: 100px auto 100px 100px;
 `
 
+const ControlButtonElem = styled.div`
+    Cusor: pointer;
+    ${props => props.active && css`
+        color: blue;
+        text-shadow: 0px 0px 60px #03ff03;
+    `}
+`
+
+function toProperCase(lower){
+    return (
+        lower.charAt(0).toUpperCase() + lower.substr(1)
+        );
+}
+
+function ControlButton({name, active}){
+    return (
+        <ControlButtonElem active={active}>
+            {toProperCase(name)}
+        </ControlButtonElem>
+    )
+}
+
 export default function(){
-    return <Bar>
-        <div> EZDashboard</div>
+    return (
+    <Bar>
+        <Logo> EZDashboard</Logo>
         <div/>
-        <div> Dashboard </div>
-        <div> Settings </div>
+        <ControlButton  name="dashboard"/>
+        <ControlButton  name="settings"/>
     </Bar>
+    )
 }
